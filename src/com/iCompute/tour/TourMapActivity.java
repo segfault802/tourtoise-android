db531c1f153404f7a206cc9958a11942af3987f7
Steven <steven@Tickles.(none)>
2011-10-31 10:59:17 -0400

    initial commit

---
+++ src/com/iCompute/tour/TourMapActivity.java
@@ -0,0 +1,13 @@
+package com.iCompute.tour;
+
+import com.google.android.maps.MapActivity;
+
+public class TourMapActivity extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
