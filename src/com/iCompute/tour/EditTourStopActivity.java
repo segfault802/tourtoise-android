d7b69498be1a9682c19b17290ef745447c2b5c0b
Steven Deeney <vtboarder89@gmail.com>
2011-10-31 19:17:42 -0400

    got the additional options button to expand and collapse options and view scrolls also added age_range array to strings.xml and set up age spinners

--- src/com/iCompute/tour/EditTourStopActivity.java
+++ src/com/iCompute/tour/EditTourStopActivity.java
@@ -4,19 +4,25 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.ViewStub;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class EditTourStopActivity extends Activity {
 
 	private Boolean optionsExpanded=false;
-	
+	private Button addOptButton;
+	private View optionsStub;
+	private Spinner ageMin;
+	private Spinner ageMax;
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.edit_stop_layout);
 		View map = findViewById(R.id.mapViewHolder);
-		Button addOptButton = (Button) findViewById(R.id.editStopAddOptButton);
+		addOptButton = (Button) findViewById(R.id.editStopAddOptButton);
 		
 		addOptButton.setOnClickListener(new OnClickListener(){
 			@Override
@@ -26,8 +32,39 @@ public class EditTourStopActivity extends Activity {
 		});
 	}
 	
+	
+	
 	private void toggleOptions()
 	{
-		
+		if(!optionsExpanded)
+		{
+			if(optionsStub==null)
+			{
+				optionsStub = ((ViewStub)findViewById(R.id.additionalOptionStub)).inflate();
+				
+				ageMin = (Spinner) findViewById(R.id.editStopMinAgeSpinner);
+				ageMax = (Spinner) findViewById(R.id.editStopMaxAgeSpinner);
+				ArrayAdapter<CharSequence> ageMinAdapt = ArrayAdapter.createFromResource(this, R.array.age_range, android.R.layout.simple_spinner_item);
+				ArrayAdapter<CharSequence> ageMaxAdapt = ArrayAdapter.createFromResource(this, R.array.age_range, android.R.layout.simple_spinner_item);
+				
+				ageMinAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			    ageMin.setAdapter(ageMinAdapt);
+			    ageMaxAdapt.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+			    ageMax.setAdapter(ageMaxAdapt);
+				
+			}
+			else
+			{
+				optionsStub.setVisibility(View.VISIBLE);
+			}
+			optionsExpanded=true;
+			addOptButton.setText("Hide Additional Options");
+		}
+		else
+		{
+			optionsStub.setVisibility(View.GONE);
+			optionsExpanded=false;
+			addOptButton.setText("Show Additional Options");
+		}
 	}
 }
