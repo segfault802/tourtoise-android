f0a422fcd54465e457de4b1800eaf24537563d86
Steven Deeney <vtboarder89@gmail.com>
2011-11-16 13:30:05 -0500

    added backgrounds to all layouts

--- gen/com/iCompute/tour/R.java
+++ gen/com/iCompute/tour/R.java
@@ -15,7 +15,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int background_dots=0x7f020000;
+        public static final int icon=0x7f020001;
     }
     public static final class id {
         public static final int EditStopsListMyLocButton=0x7f06002c;
