9688873c2d162ae9066e67b4c7e698917ce82b9e
Steven Deeney <vtboarder89@gmail.com>
2011-12-07 19:27:29 -0500

    organized toursManager

--- src/com/iCompute/tour/backend/ToursManager.java
+++ src/com/iCompute/tour/backend/ToursManager.java
@@ -2,12 +2,9 @@ package com.iCompute.tour.backend;
 
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.LinkedList;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -36,25 +33,108 @@ public class ToursManager {
 	public Tour getTour(long tourID) {
 		return mTours.getTourByID(tourID);
 	}
-
-	public void updateTour(long tourID, String name, String description,
-			String tags, boolean isWalk) {
-		Tour t = getTour(tourID);
-		t.update(name, description, tags, isWalk);		
+	
+	public Tour getTourFromServer(long tourID)
+	{
+		//TODO get tour from server
+		//DO NOT SAVE LOCALLY
+		return null;
+	}
+	
+	public boolean downloadTourFromServer(long tourID)
+	{
+		//TODO save tour locally
+		return false;
 	}
 
-	public void addTour(Tour tour) {
-		mTours.add(tour);
-	}
-
-	public void publishToServer(long tourID) {
-		// TODO Auto-generated method stub
-		
+	public Stop getStop(long tourID, long stopID) {
+		return mTours.getTourByID(tourID).getStops().getStop(stopID);
 	}
 
 	public StopList getTourStops(long tourID) {
 		return mTours.getTourByID(tourID).getStops();
 	}
+
+	public Media getMediaForTour(long tourID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<TourHeader> searchServerTours(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ArrayList<TourHeader> getLocalTours() {
+		return mTours.getTourHeaders();
+	}
+	
+	public String[] getTourStopNames(long tourID) {
+		return (String[]) mTours.getTourByID(tourID).getStops().getStopNames().toArray();
+	}
+
+	public Media getMediaForStop(long tourID, long stopID) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public Stop getTemporaryStop(long tourID) {
+		mTempStop=new Stop(tourID);
+		
+		return mTempStop;
+	}
+
+	public Tour getTemporaryTour() {
+		mTempTour = new Tour();
+		
+		return mTempTour;
+	}
+	
+
+	public void updateTour(long tourID, String name, String description,
+			String tags, boolean isWalk) {
+		//TODO check if is temp
+		getTour(tourID).update(name, description, tags, isWalk);		
+	}
+
+	public void addTour(Tour tour) {
+		//TODO test if is tempTour
+		mTours.add(tour);
+		
+	}
+	public void addTourStop(long tourID, Stop stop) {
+		//TODO test if is tempStop
+		mTours.getTourByID(tourID).addStop(stop);
+	}
+
+	public void updateStop(long tourID, Stop stop) {
+		//TODO check if is temp
+		mTours.getTourByID(tourID).updateStop(stop);
+	}
+
+	public void discardTemporaryStop(long stopID) {
+		mTempStop=null;
+	}
+
+	public void discardTemporaryTour(long tourID) {
+		mTempTour=null;
+		
+	}
+
+	public void updateTour(long tourID, Tour tour) {
+		if(tourID==mTempTour.mID)
+		{
+			mTours.add(tour);
+		}
+		else
+			mTours.getTourByID(tourID).update(tour);
+	}
+
+	
+	public void saveTemporaryTour(long tourID, Tour mTour) {
+		mTours.add(mTour);
+	}
+
 
 	public void cacheTour(long tourID) {
 		//to prevent a tour from being released from memory when activities are using it
@@ -69,81 +149,25 @@ public class ToursManager {
 		// TODO Auto-generated method stub
 		
 	}
-
-	public Stop getStop(long tourID, long stopID) {
-		return mTours.getTourByID(tourID).getStops().getStop(stopID);
-	}
-
-	public void addTourStop(long tourID, Stop stop) {
-		mTours.getTourByID(tourID).addStop(stop);
-	}
-
-	public void updateStop(long tourID, Stop stop) {
-		mTours.getTourByID(tourID).updateStop(stop);
-	}
-
-	public void discardTemporaryStop(long stopID) {
-		mTempStop=null;
-	}
-
-	public Stop getTemporaryStop(long tourID) {
-		mTempStop=new Stop(tourID);
-		
-		return mTempStop;
-	}
-
-	public Tour getTemporaryTour() {
-		mTempTour = new Tour();
-		
-		return mTempTour;
-	}
 	
-	public void updateTour(long tourID, Tour tour) {
-		if(tourID==mTempTour.mID)
-		{
-			mTours.add(tour);
-		}
-		else
-			mTours.getTourByID(tourID).update(tour);
-	}
+	
 
-	public void saveTemporaryTour(long tourID, Tour mTour) {
-		mTours.add(mTour);
-	}
-
-	public void discardTemporaryTour(long tourID) {
+	public void publishToServer(long tourID) {
 		// TODO Auto-generated method stub
 		
 	}
 
-	public Media getMediaForTour(long tourID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	public ArrayList<TourHeader> searchServerTours(String string) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public ArrayList<TourHeader> getLocalTours() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public String[] getTourStopNames(long tourID) {
-		return (String[]) mTours.getTourByID(tourID).getStops().getStopNames().toArray();
-	}
-
-	public Media getMediaForStop(long tourID, long stopID) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	
 	/*
 	 * File IO stuff
 	 */
+	
+	public void saveAllData(Context c)
+	{
+		saveToursList(c);
+		saveTours(c);
+	}
+	
 	public void saveToursList(Context c)
 	{
 		JSONArray array=new JSONArray();
@@ -153,6 +177,7 @@ public class ToursManager {
 		}
 		saveFile(c, "toursList", array.toString());
 	}
+	
 	public void saveTours(Context c)
 	{
 		for(Tour tour:mTours)
@@ -160,6 +185,7 @@ public class ToursManager {
 			saveTour(c, tour);
 		}
 	}
+	
 	public void saveTour(Context c, long id)
 	{
 		saveTour(c, mTours.getTourByID(id));
